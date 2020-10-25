25c71f6e4976c29c09993d97b8fbad428d73c3a5
Alexsander Brantsevich <lexsander.red.34@gmail.com>
2020-10-25 23:10:42 +0300

    add: code

--- src/index.js
+++ src/index.js
@@ -39,6 +39,26 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+     let  letters = expr.match(/.{1,10}/g),
+     encodedString = '';
+     letters.forEach(function(val) {
+         if(val === '**********') {let i= 0
+            i++;
+        }
+         if(val === '**********') {
+             encodedString += ' ';
+         } else {
+             let lett = val.match(/.{1,2}/g),
+                 morseString = '';
+             lett.forEach(function(val) {
+             if(val !== '00') {
+                 val === '10' ? morseString += '.' : morseString += '-';
+             }
+             })
+             encodedString += MORSE_TABLE[morseString];
+         }
+     })
+ return encodedString;
 }
 
 module.exports = {
